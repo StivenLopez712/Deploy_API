72eae1b50bc7fabeebac04833520e1541ffb554e
StivenLopez712 <jstivenlopezm@gmail.com>
2023-11-28 01:41:55 -0500

    add fun 3

--- main.py
+++ main.py
@@ -29,6 +29,18 @@ def user_for_genre(genero: str):
               "Horas jugadas": playtime_year.to_dict(orient='records')}
     return result
 
+
+# Función 3
+@app.get('/UsersRecommend')
+def users_recommend(año: int):
+    merged_df = pd.merge(DF_GAMES, DF_REVIEWS, on='Item_Id')
+    year_df = merged_df[merged_df['Year_Posted'] == año]
+    positive_neutral_df = year_df[(year_df['Sentiment_Analysis'].isin([1, 2])) & (year_df['Recommend'])]
+    top_games = positive_neutral_df.groupby('App_Name')['Recommend'].sum().reset_index()
+    top_games = top_games.sort_values(by='Recommend', ascending=False).head(3)
+    result_list = [{"Puesto {}: ".format(i+1): game} for i, game in enumerate(top_games['App_Name'])]
+    return result_list
+
 # Función 5
 @app.get('/sentiment_analysis')
 def sentiment_analysis(desarrolladora: str):
